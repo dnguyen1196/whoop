69a0b408ce3edf4ebd0b1923b61768703be730a3
ducnguyentufts <manh.nguyen@tufts.edu>
2016-10-15 00:47:02 -0400

    Ysy first

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var express = require('express');
+var express = require("express");
+var morgan = require("morgan");
+var cors = require("cors");
+
+// Own modules
+var EventSearch = require("facebook-events-by-location-core");
+
+// Create the Express object
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+// Use morgan for logging
+app.use(morgan("combined"));
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -10,11 +19,24 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+        response.sendFile("./public/index.html");
 });
+
+// Instantiate CORS whitelist
+
+// var es = new EventSearch({
+// "accessToken":"1149596415132442|hlbvMEjn4RxBx6YropCuZ7LlJKw",
+//     "lat": 40.710803,
+//     "lng": -73.964040
+// });
+//
+// es.search().then(function (events) {
+//     console.log(JSON.stringify(events));
+// }).catch(function (error) {
+//     console.error(JSON.stringify(error));
+// });
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
