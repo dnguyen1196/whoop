10666f16738ac2805ff70cc46720af6399f52614
AlexTNguyen <alex.nguyen@tufts.edu>
2016-10-15 04:37:16 -0400

    not sure

--- public/fb.js
+++ public/fb.js
@@ -3,7 +3,6 @@ $(function() {
     	var data = {};
     	data.lat = $("#lat").val();
     	data.lng = $("#lng").val();
-    	console.log(data.lat);
         $.ajax({
             type: 'post',
             url: '/events',
@@ -14,3 +13,34 @@ $(function() {
         });
     });
 });
+
+// function centerMap() {
+// 	console.log("hi");
+// 	console.log("ya1u");
+// 	var LatLng = new google.maps.LatLng(lat, lng);
+// 	var lat = document.getElementById("lat").val();
+// 	var lng = document.getElementById("lng").val();
+// 	var map = new google.maps.Map(document.getElementById("map"), {
+//       styles: [{
+//         stylers: [
+//           { hue: '#5fade7' },
+//           { visibility: 'simplified' },
+//           { gamma: 0.5 },
+//           { weight: 0.5 }
+//         ]
+//       }, {
+//         elementType: 'labels',
+//         stylers: [{ visibility: 'off' }]
+//       }, {
+//         featureType: 'water',
+//         stylers: [{ color: '#5fade7' }]
+//       }],
+//       mapTypeControl: false,
+//       streetViewControl: false,
+//       center: LatLng
+
+//     });
+// 	console.log("hfuioasd");
+//     map.panTo(LatLng);
+//     console.log("yau");
+// }
