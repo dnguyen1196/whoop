ac8ef38583f31b791c6edd510573930b1cf8ab23
ducnguyentufts <manh.nguyen@tufts.edu>
2016-10-15 07:36:22 -0400

    Work with tripadvisor api

--- public/map.js
+++ public/map.js
@@ -1,8 +1,9 @@
+var map;
 function initAutocomplete() {
-        var map = new google.maps.Map(document.getElementById('googlemap'), {
-          center: {lat: -33.8688, lng: 151.2195},
-          zoom: 13,
-          mapTypeId: 'roadmap'
+        map = new google.maps.Map(document.getElementById('googlemap'), {
+                center: {lat: -33.8688, lng: 151.2195},
+                zoom: 13,
+                mapTypeId: 'roadmap'
         });
 
         // Create the search box and link it to the UI element.
@@ -12,55 +13,142 @@ function initAutocomplete() {
 
         // Bias the SearchBox results towards current map's viewport.
         map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
+                searchBox.setBounds(map.getBounds());
         });
 
         var markers = [];
         // Listen for the event fired when the user selects a prediction and retrieve
         // more details for that place.
         searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
+                var places = searchBox.getPlaces();
 
-          if (places.length == 0) {
-            return;
-          }
+                if (places.length == 0) {
+                        return;
+                }
+                // Clear out the old markers.
+                markers.forEach(function(marker) {
+                        marker.setMap(null);
+                });
+                markers = [];
+                // For each place, get the icon, name and location.
+                var bounds = new google.maps.LatLngBounds();
+                places.forEach(function(place) {
+                        if (!place.geometry) {
+                                console.log("Returned place contains no geometry");
+                                return;
+                        }
+                        var icon = {
+                                url: place.icon,
+                                size: new google.maps.Size(71, 71),
+                                origin: new google.maps.Point(0, 0),
+                                anchor: new google.maps.Point(17, 34),
+                                scaledSize: new google.maps.Size(25, 25)
+                        };
+                        var lat = place.geometry.location.lat();
+                        var lng = place.geometry.location.lng();
+                        get_businesses_info(lat, lng, "attractions");
+                        get_businesses_info(lat, lng, "restaurants");
+                        // Create a marker for each place.
+                        markers.push(new google.maps.Marker({
+                                map: map,
+                                icon: icon,
+                                title: place.name,
+                                position: place.geometry.location
+                        }));
 
-          // Clear out the old markers.
-          markers.forEach(function(marker) {
-            marker.setMap(null);
-          });
-          markers = [];
-
-          // For each place, get the icon, name and location.
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            if (!place.geometry) {
-              console.log("Returned place contains no geometry");
-              return;
-            }
-            var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-              map: map,
-              icon: icon,
-              title: place.name,
-              position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
+                        if (place.geometry.viewport) {
+                                // Only geocodes have viewport.
+                                bounds.union(place.geometry.viewport);
+                        } else {
+                                bounds.extend(place.geometry.location);
+                        }
+                });
+                map.fitBounds(bounds);
         });
-      }
+}
+
+
+var TRIP_API_KEY = "89DE2CFC0C1C43978B484B55F9A514EC";
+
+
+function get_businesses_info(lat, lng, type){
+        var url = "http://api.tripadvisor.com/api/partner/2.0/map/"
+        url += lat + ","+ lng + "/" + type + "?";
+        url += 'key=' + TRIP_API_KEY;
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onload = function (e) {
+                if (xhr.readyState === 4) {
+                        if (xhr.status === 200) {
+                                parseReturnData(xhr.responseText, type);
+                        } else {
+                                console.error(xhr.statusText);
+                        }
+                }
+        };
+        xhr.send(null);
+}
+
+function parseReturnData(xhr, type){
+        var text = JSON.parse(xhr);
+        var data = text["data"];
+        for (var i = 0; i < data.length; i++) {
+                var address = data[i]["address_obj"];
+                var str = address["address_string"];
+                performGeoCoding(str, type);
+        }
+}
+GEO_CODE_API = "AIzaSyDZ6smFYqvu6DIpeKTa2VIlolN-4yIWW_A";
+
+function performGeoCoding(address, type) {
+        var url = "https://maps.googleapis.com/maps/api/geocode/json?address="
+        var formated = address.split(" ").join("+");
+        url += formated;
+        url += ("&key=" + GEO_CODE_API);
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onload = function (e) {
+                if (xhr.readyState === 4) {
+                        if (xhr.status === 200) {
+                                parseGeoCode(xhr.responseText, type);
+                        } else {
+                                console.error(xhr.statusText);
+                        }
+                }
+        };
+        xhr.send(null);
+}
+
+function parseGeoCode(text, type) {
+        var data = JSON.parse(text);
+        var results = data["results"];
+        results = results[0];
+
+        var geometry = results["geometry"];
+        var coordinate = geometry["location"];
+        var lat = coordinate["lat"];
+        var lng = coordinate["lng"];
+        addMarkerToMap(lat, lng, type);
+}
+
+function addMarkerToMap(lat, lng, type) {
+        myLatLng = {lat:lat, lng:lng};
+
+        if (type=="restaurants"){
+                image_url = "./restaurant.png";
+        } else {
+                image_url ="./landmark.png";
+        }
+        var image = {
+                url: image_url,
+                scaledSize: new google.maps.Size(30, 30),
+                origin: new google.maps.Point(0,0), // origin
+                anchor: new google.maps.Point(0,0), // a
+        };
+
+        var marker = new google.maps.Marker({
+                position: myLatLng,
+                icon: image,
+                map: map,
+        });
+}
